c1b1cbb4b3e1264561593665ba0b90970241f6f9
mistergeneric <andrewmcneill1992@gmail.com>
2020-03-16 16:42:00 +0000

    Updates

---
+++ precache-manifest.e93e3c60d5b76e6f8b6a8812fbc5c284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7309d9c14a5956787047d4d9c56726b7",
+    "url": "/monsters-emporium/index.html"
+  },
+  {
+    "revision": "9f744bbacc7723c7d524",
+    "url": "/monsters-emporium/static/css/main.6b93b670.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-emporium/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-emporium/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f744bbacc7723c7d524",
+    "url": "/monsters-emporium/static/js/main.7c53711e.chunk.js"
+  },
+  {
+    "revision": "d8ba6d8c068600887c2a",
+    "url": "/monsters-emporium/static/js/runtime-main.288fe38d.js"
+  }
+]);
